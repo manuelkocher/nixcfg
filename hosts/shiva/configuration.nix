d9f335ea6495f8d9cb242f8734d9ed462b562543
Manuel Kocher <manuelkocher98@gmail.com>
2026-03-13 17:55:30 +0100

    Add host shiva for lenovo yoga

---
+++ hosts/shiva/configuration.nix
@@ -0,0 +1,59 @@
+# Lenovo Yoga
+
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{
+  config,
+  pkgs,
+  userLogin,
+  userNameLong,
+  userEmail,
+  inputs,
+  ...
+}:
+
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ../../modules/mixins/users.nix
+    ../../modules/mixins/desktop.nix
+    ../../modules/mixins/git.nix
+    ../../modules/mixins/audio.nix
+    ../../modules/mixins/openssh.nix
+    ../../modules/mixins/remote-store-cache.nix
+    ../../modules/mixins/librewolf.nix
+  ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.edk2-uefi-shell.enable = false; # enable uefi shell
+
+  networking.hostName = "shiva"; # Define your hostname.
+
+  # Add the openconnect plugin for NetworkManager
+  networking.networkmanager.plugins = with pkgs; [
+    networkmanager-openconnect
+  ];
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    wireshark
+    stable.sage
+  ];
+
+  programs.nix-ld.enable = true;
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  # We have enough RAM
+  zramSwap.enable = false;
+}
